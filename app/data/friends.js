117f051a6827d5664b2ac367c8ae1366e678502c
Andrew Andersen <andersac88@gmail.com>
2018-12-05 23:45:42 -0600

    changed friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,52 +1,40 @@
 let friendArray = [
-    {
-        "name": "Homer Simpson",
-        "photo": "https://upload.wikimedia.org/wikipedia/uk/0/02/Homer_Simpson_2006.png",
-        "scores": [
-        "5",
-        "1",
-        "4",
-        "4",
-        "5",
-        "1",
-        "2",
-        "5",
-        "4",
-        "1"
-        ]
-        },
-        {
-        "name": "Barney  Gumble",
-        "photo": "https://vignette.wikia.nocookie.net/scratchpad/images/b/b5/BarneyGumble.png/revision/latest?cb=20130313114535",
-        "scores": [
-        "4",
-        "2",
-        "5",
-        "1",
-        "3",
-        "2",
-        "2",
-        "1",
-        "3",
-        "2"
-        ]
-        },
-        {
-        "name": "Lenny Leonard",
-        "photo": "https://vignette.wikia.nocookie.net/simpsons/images/a/ae/Lenny_Leonard.png/revision/latest?cb=20180311092231",
-        "scores": [
-        "5",
-        "2",
-        "2",
-        "2",
-        "4",
-        "1",
-        "3",
-        "2",
-        "5",
-        "5"
-        ]
-        }
+  {
+    name: "Lars",
+    photo:
+      "https://images.pexels.com/photos/316680/pexels-photo-316680.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=300",
+    scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+  },
+  {
+    name: "Samantha",
+    photo:
+      "https://images.pexels.com/photos/301290/pexels-photo-301290.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=300",
+    scores: ["1", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
+  },
+  {
+    name: "Lance",
+    photo:
+      "https://images.pexels.com/photos/447189/pexels-photo-447189.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=300",
+    scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
+  },
+  {
+    name: "Kyle",
+    photo:
+      "https://images.pexels.com/photos/450271/pexels-photo-450271.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=300",
+    scores: ["4", "1", "3", "5", "2", "3", "1", "4", "5", "1"]
+  },
+  {
+        name: "Nicole",
+        photo:
+          "https://images.pexels.com/photos/1037915/pexels-photo-1037915.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=300",
+        scores: ["1", "1", "3", "3", "2", "2", "1", "4", "2", "4"]
+      },
+  {
+    name: "Jordan",
+    photo:
+      "https://images.pexels.com/photos/1036627/pexels-photo-1036627.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=300",
+    scores: ["5", "5", "3", "2", "1", "1", "1", "4", "4", "2"]
+  }
 ];
 
 module.exports = friendArray;
