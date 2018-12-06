0d66fc7c8b6a7de11af1b956cf3e90321db50aae
Andrew Andersen <andersac88@gmail.com>
2018-12-05 22:16:26 -0600

    removed console.logs

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,27 +1,21 @@
 let friendData = require("../data/friends");
 
-
-module.exports = (app) => {
-app.get("/api/friends", (req, res) => {
+module.exports = app => {
+  app.get("/api/friends", (req, res) => {
     res.json(friendData);
-});
+  });
 
-app.post("/api/friends", (req, res) => {
-   let userArray = req.body.scores;
+  app.post("/api/friends", (req, res) => {
+    let userArray = req.body.scores;
     let a = userArray.map(a => +a);
     let diffArray = [];
-   for (i = 0; i < friendData.length; i++) {
-     let tempB = friendData[i].scores.map(a => +a);
-     console.log(tempB);
-     let temp = a.map((value, index) => Math.abs(value - tempB[index]))
-     console.log(temp);
-    let reducedArr = temp.reduce((sum, value) => sum + value, 0);    
-    console.log(reducedArr);
-    diffArray.push(reducedArr);
-   } console.log(diffArray);
-   let picker = diffArray.indexOf(Math.min(...diffArray));
-   console.log(picker);
-   console.log(friendData[picker])
-   res.json(friendData[picker]);
-});
-}
+    for (i = 0; i < friendData.length; i++) {
+      let tempB = friendData[i].scores.map(a => +a);
+      let temp = a.map((value, index) => Math.abs(value - tempB[index]));
+      let reducedArr = temp.reduce((sum, value) => sum + value, 0);
+      diffArray.push(reducedArr);
+    }
+    let picker = diffArray.indexOf(Math.min(...diffArray));
+    res.json(friendData[picker]);
+  });
+};
